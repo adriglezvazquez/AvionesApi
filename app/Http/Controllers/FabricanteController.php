cd4dc09697805fec77e2dcdaddb76ee7a1f3dbbd
adriglezvazquez <adriglezvazquez@gmail.com>
2015-04-15 21:39:47 +0200

    Revert "Revert "Creamos la ruta de store""
    This reverts commit d8f4e3d6ed54def365c57654296622ae129e7956.

--- app/Http/Controllers/FabricanteController.php
+++ app/Http/Controllers/FabricanteController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
+use Response;
+
 //cargamos fabricante porque lo usamos mas abajo
 use App\Fabricante;
 
@@ -41,9 +44,30 @@ class FabricanteController extends Controller {
 	 *
 	 * @return Response
 	 */
-	/* 	public function store() {
-	  //
-	  } */
+	public function store(Request $request) {
+		//
+		//metodo llamada al hacer un post
+		//comprobamos que recibimos todos los campos
+
+		if (!$request->input('nombre') || !$request->input('direccion') || !$request->input('telefono')) {
+			return response()->json([
+						'errors' => Array(['code' => 422, 'message' => 'Faltan datos necesarios para procesar el alta'
+							])], 422);
+		}
+
+		//insertamos los datos recibidos en la tabla 
+		//devolvemos el codigo 201 que significa que se han insertados los datos
+		
+		$nuevoFabricante = Fabricante::create($request->all());
+		
+		//devolvemos la respuesta Http 201 (created) + los datos del nuevo fabricante + una cabecera de Location
+		
+		$respuesta = Response::make(json_encode(['data'=>$nuevoFabricante]),201)->header('Location','http://www.dominio.local/fabricante/'.$nuevoFabricante->id)->header('Content-Type','application/json');
+		
+		return $respuesta;
+		
+		
+	}
 
 	/**
 	 * Display the specified resource.
@@ -59,11 +83,11 @@ class FabricanteController extends Controller {
 			//se devuelve un error con los errores detectados y codigo 404
 
 			return response()->json([
-						'errors' => Array(['code' => 404, 'message' => 'No se encuentra un fabricante con ese codigo'
-							])], 404);
+						'errors' => Array(['code' => 422, 'message' => 'No se encuentra un fabricante con ese codigo'
+							])], 422);
 		}
-		
-		return response()-> json(['status'=>'ok','data'=>$fabricante],200);
+
+		return response()->json(['status' => 'ok', 'data' => $fabricante], 200);
 	}
 
 	/**
